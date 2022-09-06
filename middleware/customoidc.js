4f0dd76f24a50ee910e2a133cca49c053fd39208
mbracer84 <96140905+mbracer84@users.noreply.github.com>
2022-09-06 13:14:22 +0200

    Delete customoidc.js

--- middleware/customoidc.js
+++
@@ -1,9 +0,0 @@
-export default function (context) {
-  //    const endpoints = context.$auth.strategies.oidc;
-   var host = context.req.headers.host;
-   console.log('HOST: ' + host)
-
-  //    for (const key in endpoints) {
-  //      console.log(key);
-  //    }
-  }
